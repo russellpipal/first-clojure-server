cbc285836b664910e5eb02bb8653e7192fed41f7
russellpipal <russell.pipal@gmail.com>
2016-09-16 17:30:05 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# first-clojure-server
+
+Setting up my first server in Clojure.
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ test/first_clojure_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns first-clojure-server.core-test
+  (:require [clojure.test :refer :all]
+            [first-clojure-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
